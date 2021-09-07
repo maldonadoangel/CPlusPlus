1caa4144b5669dcb00122015a704b30dedd838de
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-07 16:31:41 -0500

    Ejercicio #2 expresiones

---
+++ expresiones/expresiones2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    float a,b,c,d, resultado = 0.0;
+
+    cout<<"Digite el valor de a: "<<endl;
+    cin>>a;
+    cout<<"Digite el valor de b: "<<endl;
+    cin>>b;
+    cout<<"Digite el valor de c: "<<endl;
+    cin>>c;
+    cout<<"Digite el valor de d: "<<endl;
+    cin>>d;
+
+    resultado = (a+b)/(c+d);
+    cout<<"El resultado de la formula (a+b)/(c+d) es: "<<resultado<<endl;
+
+    return 0;
+}
