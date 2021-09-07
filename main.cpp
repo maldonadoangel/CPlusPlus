eb693adca477fdb148a9fc339c6750466a63553b
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-07 12:57:43 -0500

    Tipos de variables basicos

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main(){
-    String nombre = "Angel Morales";
     cout<<"Hola mundo desde c++"<<endl;
     return 0;
 }
---
+++ variables basicas/tipos_basicos.cpp
@@ -0,0 +1,19 @@
+//Tipos de datos basicos en c++ 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edad = 23;
+    float flotante = 23.9;
+    double mayor = 16.3416;
+    char letra = 'a';
+
+    cout<<edad<<endl;
+    cout<<flotante<<endl;
+    cout<<mayor<<endl;
+    cout<<letra<<endl;
+
+    return 0;
+}
