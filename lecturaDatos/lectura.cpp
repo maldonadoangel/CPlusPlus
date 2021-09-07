a7f321e512c983b5efe98d10a2d428f536056032
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-07 13:04:12 -0500

    Entrada de datos

---
+++ lecturaDatos/lectura.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edad;
+
+    cout<<"Ingrese su edad "<<endl;
+    cin>>edad;
+    cout<<"Tu edad es: "<<edad<<" anios"<<endl;
+
+
+    return 0;
+}
